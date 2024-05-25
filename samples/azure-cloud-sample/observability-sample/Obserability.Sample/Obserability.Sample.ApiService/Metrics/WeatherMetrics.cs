71e5d0c42e99dcec6b18771c64092ffcc94f84e9
MADALIN-PC\matei <matei.madalin1@gmail.com>
2024-05-25 18:07:53 +0300

    azure observability

---
+++ samples/azure-cloud-sample/observability-sample/Obserability.Sample/Obserability.Sample.ApiService/Metrics/WeatherMetrics.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Obserability.Sample.ApiService.Metrics;
+
+public class WeatherMetrics
+{
+    private readonly Counter<int> _serviceCalls;
+    private readonly Counter<int> _hotForecasts;
+
+    public WeatherMetrics(IMeterFactory meterFactory)
+    {
+        var meter = meterFactory.Create("Obserability.Sample.ApiService");
+
+        _serviceCalls = meter.CreateCounter<int>(name: "observability.apiService.weather.service_calls",
+            unit: "{calls}",
+            description: "Number of times the API service is being called to list all weather forecasts.");
+
+        _hotForecasts = meter.CreateCounter<int>("observability.apiService.weather.hot", unit: "{celsiusDegrees}",
+            description: "Weather forecasts above 30 celsius degrees");
+    }
+
+    public void ServiceCalls(int quantity)
+    {
+        _serviceCalls.Add(quantity);
+    }
+
+    public void HotForecast()
+    {
+        _hotForecasts.Add(1);
+    }
+}
